3485e2397bb482dab938e881de05623aefe33430
Julio Cesar Meinerz <julio.meinerz@live.com>
2017-01-06 11:54:50 -0200

    video search

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -16,7 +16,7 @@ const VideoDetail = ({video}) => {
 			</div>
 			<div className="details">
 				<div> {video.snippet.title} </div>
-				<div> {video.snippet.description} </div>
+				<div className="video-description"> {video.snippet.description} </div>
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyCE_NielMAdDm6RZO6fldUF9ptQmZ03xFA";
-
-
 
 class App extends Component {
 	constructor(props) {
@@ -18,22 +17,31 @@ class App extends Component {
 			videos: [],
 			selectedVideo: null
 		};
+		this.videoSearch('surfboards');
 
-		YTSearch({key: API_KEY, term :'4k'}, (videos) => {
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term : term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
 			});
 		});
 	}
+
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+		
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.selectedVideo}/>
-				<VideoList 
-					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-					videos={this.state.videos}/>
+				<SearchBar onSearchTermChange={videoSearch}  />
+				<div className="row">
+					<VideoDetail video={this.state.selectedVideo}/>
+					<VideoList 
+						onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+						videos={this.state.videos}/>
+				</div>
 			</div>
 		);		
 	}	
